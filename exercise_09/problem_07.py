169ada88d17b18712b12c7c91c2f6e5b92ac802b
KashanAdnan-Coder <computertechhub0@gmail.com>
2024-07-24 22:46:48 -0700

    changes

--- exercise_09/problem_07.py
+++ exercise_09/problem_07.py
@@ -4,8 +4,11 @@
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 
-if month == 2 and year % 4 :
-    print("February has 29 days")
+if month == 2:
+    if year % 4 == 0:
+        print(f"{month} has 29 days")
+    else:
+        print(f"{month} has 28 days")
 elif month in [4, 6, 9, 11]:
     print(f"{month} has 30 days")
 else:
